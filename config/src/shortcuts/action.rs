c445104b51c5bc6ec012a2abde953ed14eaef004
Michael Aaron Murphy <michael@mmurphy.dev>
2024-06-25 16:57:56 +0200

    feat(config): document APIs and add methods for cosmic-settings

--- config/src/shortcuts/action.rs
+++ config/src/shortcuts/action.rs
@@ -2,61 +2,134 @@
 
 use serde::{Deserialize, Serialize};
 
+/// An operation which may be bound to a keyboard shortcut.
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum Action {
-    Terminate,
-    Debug,
+    /// Close the active window
     Close,
-    Workspace(u8),
-    NextWorkspace,
-    PreviousWorkspace,
+
+    /// Show a debug overlay, if enabled in the compositor build
+    Debug,
+
+    /// Change focus to the window or workspace in the given direction
+    Focus(FocusDirection),
+
+    /// Change focus to the last workspace
     LastWorkspace,
-    MoveToWorkspace(u8),
-    MoveToNextWorkspace,
-    MoveToPreviousWorkspace,
-    MoveToLastWorkspace,
-    SendToWorkspace(u8),
-    SendToNextWorkspace,
-    SendToPreviousWorkspace,
-    SendToLastWorkspace,
 
-    NextOutput,
-    PreviousOutput,
-    MoveToNextOutput,
-    MoveToPreviousOutput,
-    SendToNextOutput,
-    SendToPreviousOutput,
-    SwitchOutput(Direction),
-    MoveToOutput(Direction),
-    SendToOutput(Direction),
+    /// Maximize the active window
+    Maximize,
 
+    /// Migrate the active workspace to the next output
     MigrateWorkspaceToNextOutput,
-    MigrateWorkspaceToPreviousOutput,
+
+    /// Migrate the active workspace to the output in the given direction
     MigrateWorkspaceToOutput(Direction),
 
-    Focus(FocusDirection),
+    /// Migrate the active workspace to the previous output
+    MigrateWorkspaceToPreviousOutput,
+
+    /// Minimize the active window
+    Minimize,
+
+    /// Move a window in the given direction
     Move(Direction),
 
-    ToggleOrientation,
+    /// Move a window to the last workspace
+    MoveToLastWorkspace,
+
+    /// Move a window to the next output
+    MoveToNextOutput,
+
+    /// Move a window to the next workspace
+    MoveToNextWorkspace,
+
+    /// Move a window to the given output
+    MoveToOutput(Direction),
+
+    /// Move a window to the previous output
+    MoveToPreviousOutput,
+
+    /// Move a window to the previous workspace
+    MoveToPreviousWorkspace,
+
+    /// Move a window to the given workspace
+    MoveToWorkspace(u8),
+
+    /// Change focus to the next output
+    NextOutput,
+
+    /// Change focus to the next workspace
+    NextWorkspace,
+
+    /// Change the orientation of a tiling group
     Orientation(Orientation),
 
-    ToggleStacking,
-    ToggleTiling,
-    ToggleWindowFloating,
-    ToggleSticky,
+    /// Change focus to the previous output
+    PreviousOutput,
+
+    /// Change focus to the previous workspace
+    PreviousWorkspace,
+
+    /// Resize the active window in a given direction
+    Resizing(ResizeDirection),
+
+    /// Move a window to the last workspace
+    SendToLastWorkspace,
+
+    /// Move a window to the next output
+    SendToNextOutput,
+
+    /// Move a window to the next workspace
+    SendToNextWorkspace,
+
+    /// Move a window to the output in the given direction
+    SendToOutput(Direction),
+
+    /// Move a window to the previous output
+    SendToPreviousOutput,
+
+    /// Move a window to the previous workspace
+    SendToPreviousWorkspace,
+
+    /// Move a window to the given workspace
+    SendToWorkspace(u8),
+
+    /// Swap positions of the active window with another
     SwapWindow,
 
-    Resizing(ResizeDirection),
-    Minimize,
-    Maximize,
+    /// Move to an output in the given direction
+    SwitchOutput(Direction),
 
     /// Perform a common system operation
     System(System),
 
     /// Execute a command with any given arguments
     Spawn(String),
+
+    /// Stop the compositor
+    Terminate,
+
+    /// Toggle the orientation of a tiling group
+    ToggleOrientation,
+
+    /// Toggle window stacking for the active window
+    ToggleStacking,
+
+    /// Toggle the sticky state of the active window
+    ToggleSticky,
+
+    /// Toggle tiling mode of the active workspace
+    ToggleTiling,
+
+    /// Toggle between tiling and floating window states for the active window
+    ToggleWindowFloating,
+
+    /// Change focus to the given workspace ID
+    Workspace(u8),
 }
 
+/// Common system operations which may be controlled by system commands
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum System {
     /// Opens the application library
@@ -95,6 +168,7 @@ pub enum System {
     WorkspaceOverview,
 }
 
+/// Defines the direction of an operation
 #[derive(Copy, Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum Direction {
     Left,
@@ -115,6 +189,7 @@ impl std::ops::Not for Direction {
     }
 }
 
+/// Defines the direction to focus towards
 #[derive(Copy, Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum FocusDirection {
     Left,
@@ -125,12 +200,14 @@ pub enum FocusDirection {
     Out,
 }
 
+/// Defines the direction to resize towards
 #[derive(Copy, Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum ResizeDirection {
     Inwards,
     Outwards,
 }
 
+/// Defines the edge of a window to resize from
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum ResizeEdge {
     Bottom,
@@ -143,6 +220,7 @@ pub enum ResizeEdge {
     TopRight,
 }
 
+/// Tiling orientation for a tiling window group
 #[derive(Copy, Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub enum Orientation {
     Horizontal,
