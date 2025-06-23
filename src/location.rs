536ade1cf4b80bcc1e22bc91b2476211229a0798
Michael Aaron Murphy <michael@mmurphy.dev>
2025-06-23 21:00:25 +0200

    fix: keep timezone watcher alive

--- src/location.rs
+++ src/location.rs
@@ -3,6 +3,7 @@ use std::{collections::BTreeMap, io, path::Path, rc::Rc};
 use futures::{Stream, StreamExt};
 pub use geonames::GeoPosition;
 use notify::{RecursiveMode, Watcher};
+use tokio::task::JoinHandle;
 
 static GEODATA: &'static [u8] = include_bytes!("../data/timezone-geodata.bitcode-v0-6");
 
@@ -16,29 +17,42 @@ pub fn decode_geodata() -> BTreeMap<String, GeoPosition> {
     }
 }
 
-pub fn receive_timezones() -> impl Stream<Item = io::Result<String>> {
+pub fn receive_timezones() -> (JoinHandle<()>, impl Stream<Item = io::Result<String>>) {
     let (tx, rx) = tokio::sync::mpsc::channel(1);
-    let mut watcher = notify::recommended_watcher(move |result: notify::Result<notify::Event>| {
-        if matches!(
-            result.map(|event| event.kind),
-            Ok(notify::EventKind::Modify(notify::event::ModifyKind::Data(
-                _
-            )))
-        ) {
-            futures::executor::block_on(async {
-                _ = tx.send(()).await;
-            })
+
+    let timezone_path: Rc<Path> = Rc::from(Path::new("/etc/timezone").canonicalize().unwrap());
+
+    let handle = tokio::task::spawn_local({
+        let timezone_path = timezone_path.clone();
+        async move {
+            let mut watcher =
+                notify::recommended_watcher(move |result: notify::Result<notify::Event>| {
+                    if matches!(
+                        result.map(|event| event.kind),
+                        Ok(notify::EventKind::Modify(notify::event::ModifyKind::Data(
+                            _
+                        )))
+                    ) {
+                        futures::executor::block_on(async {
+                            _ = tx.send(()).await;
+                        })
+                    }
+                })
+                .unwrap();
+
+            _ = watcher.watch(&timezone_path, RecursiveMode::NonRecursive);
+
+            futures::future::pending::<()>().await;
         }
-    })
-    .unwrap();
+    });
 
-    let timezone_path = Rc::from(Path::new("/etc/timezone").canonicalize().unwrap());
-
-    _ = watcher.watch(&timezone_path, RecursiveMode::NonRecursive);
-
-    tokio_stream::wrappers::ReceiverStream::new(rx)
-        .then(move |_| {
+    let stream =
+        tokio_stream::wrappers::ReceiverStream::new(rx).then(move |_| {
             let timezone_path = timezone_path.clone();
-            async move { std::fs::read_to_string(&timezone_path).map(|contents| contents.trim().to_owned()) }
-        })
+            async move {
+                std::fs::read_to_string(&timezone_path).map(|contents| contents.trim().to_owned())
+            }
+        });
+
+    (handle, stream)
 }
